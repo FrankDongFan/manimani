90a2c21d8e879b3160ceab8364b91dfdf60bfc2e
frank <970359440@qq.com>
2022-05-08 13:26:13 +0800

    使用全局tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,4 +4,21 @@ type RecordItem = {
     type: string;
     amount: number; // 数据类型 object | string
     createdAt?: Date; // 类 / 构造函数
-  };
+}
+
+interface Window{
+  tagList:Tag[]
+}
+
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[]
+  fetch: () => Tag[]
+  create: (name: string) => 'success' | 'duplicated' // 联合类型
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  remove: (id: string) => boolean
+  save: () => void
+}
